7c6869f1c178a6e7fb4b261a8a2653a4c3104124
Егор Федоруцэ <fedorutsey@gmail.com>
2026-02-21 16:10:10 +0300

    день 1 и 2,и все задания из этих дней

--- week_intensiv/day1_magic/tasks/item_comparison.py
+++ week_intensiv/day1_magic/tasks/item_comparison.py
@@ -1,18 +1,17 @@
 class Item:
-    """ЗАДАЧА: Сравнение товаров по цене через __lt__ и по всем полям через __eq__"""
-
+    """ЗАДАЧА: Сравнение товаров по цене через lt и по всем полям через __eq__"""
     def __init__(self, name, price):
         self.name = name
         self.price = price
-
     def __lt__(self, other):
-        """Сравнение по цене (меньше)"""
         if not isinstance(other, Item):
             return NotImplemented
         return self.price < other.price
-
     def __eq__(self, other):
-        """Сравнение по всем полям"""
         if not isinstance(other, Item):
             return NotImplemented
-        return self.name == other.name and self.price == other.price
+        if self.name != other.name:
+            return False
+        else:
+            return True
+
--- week_intensiv/day2_encapsulation/tasks/valid_user.py
+++ week_intensiv/day2_encapsulation/tasks/valid_user.py
@@ -1,7 +1,17 @@
 class ValidUser:
     """ЗАДАЧА: Сеттер пароля с проверкой длины >= 8 и наличия цифр"""
-    def __init__(self, user, pwd): self.username, self._password = user, pwd
-    @property
-    def password(self): return "********"
-    @password.setter
-    def password(self, val): pass
+    def __init__(self, user, pwd):
+        self.username = user
+        self._password = pwd
+
+    def password(self):
+        return "********"
+
+    def password(self, val):
+        has_digit = False
+        for c in val:
+            if c.isdigit():
+                has_digit = True
+                break
+        if len(val) >= 8 and has_digit:
+            self._password = val
