37a128d5a8b478b7b48d85df9388796da3a6c695
Егор Федоруцэ <fedorutsey@gmail.com>
2026-02-26 23:17:49 +0300

    день 3 выполнен

--- week_intensiv/day3_lists/tasks/grade_book.py
+++ week_intensiv/day3_lists/tasks/grade_book.py
@@ -1,4 +1,25 @@
 class GradeBook:
     """ЗАДАЧА: Найти имя студента с самым высоким средним баллом"""
-    def __init__(self): self.students = {} # {"Ivan": [5, 4], "Oleg": [3]}
-    def get_best_student(self): pass
+
+    def __init__(self):
+        self.students = {}
+
+    def get_best_student(self):
+        if not self.students:
+            return None
+
+        best_student = None
+        best_average = -1
+
+        for name, grades in self.students.items():  # Исправлено: students + убран пробел
+            if grades:  # если есть оценки
+                average = sum(grades) / len(grades)
+                if average > best_average:
+                    best_average = average
+                    best_student = name
+
+        return best_student
+
+
+
+
--- week_intensiv/day3_lists/tasks/playlist.py
+++ week_intensiv/day3_lists/tasks/playlist.py
@@ -1,4 +1,11 @@
 class Playlist:
     """ЗАДАЧА: Подсчет общей длительности песен (track_list - список секунд)"""
-    def __init__(self): self.tracks = []
-    def get_duration(self): pass
+
+    def __init__(self):
+        self.tracks = []
+
+    def get_duration(self):
+        total = 0  # Начинаем с нуля
+        for track in self.tracks:  # Проходим по каждой песне
+            total += track  # Добавляем её длительность к общей
+        return total  # Возвращаем результат
