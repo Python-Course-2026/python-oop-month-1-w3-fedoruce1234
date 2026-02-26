41439b62cb38e82e46b7c687d28c6583a052a5b5
Егор Федоруцэ <fedorutsey@gmail.com>
2026-02-26 23:48:05 +0300

    день 4 задача warehouse  готова

--- week_intensiv/day4_logic/tasks/warehouse_manager.py
+++ week_intensiv/day4_logic/tasks/warehouse_manager.py
@@ -12,17 +12,29 @@ class WarehouseManager:
     2. filter_by_category(cat): возвращает список ОБЪЕКТОВ только этой категории.
     3. get_total_value(): возвращает сумму цен всех товаров на складе (float/int).
     """
+
     def __init__(self):
         self.items = []
 
     def add_product(self, product: Product):
         # Добавьте объект product в список self.items
-        pass
+        self.items.append(product)
 
     def filter_by_category(self, category: str):
         # Верните список объектов, чья категория совпадает с заданной
-        pass
+        result = []
+
+        for product in self.items:
+            if product.category == category:
+                result.append(product)
+
+        return result
 
     def get_total_value(self):
         # Посчитайте сумму цен всех товаров
-        pass
+        total = 0
+
+        for product in self.items:
+            total += product.price
+
+        return total
